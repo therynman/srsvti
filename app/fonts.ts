f59022aa1fd66c77f924f77c9c8f3d3bdadd10e9
Rayan Das <rayan@srsvti.com>
2025-02-03 18:09:12 +0530

    adding fonts

--- app/fonts.ts
+++ app/fonts.ts
@@ -2,13 +2,13 @@
 import localFont from "next/font/local";
 
 export const display = localFont({
-    src: "../public/fonts/HostGrotesk.woff2",
+    src: "../public/fonts/HostGrotesk-Regular.tff",
     variable: "--font-display",
     display: "swap",
 });
 
 export const sans = localFont({
-    src: "../public/fonts/HostGrotesk.woff2",
+    src: "../public/fonts/HostGrotesk-Regular.tff",
     variable: "--font-sans",
     display: "swap",
 });
